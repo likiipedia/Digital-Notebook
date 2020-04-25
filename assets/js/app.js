9dd13d265c750375d05c65a588d95706c8bac28d
likiipedia <46091873+likiipedia@users.noreply.github.com>
2020-04-25 16:31:47 +0100

    Homepage 0.01
    - added navbar
    - added typewriter Hello World
    - added flipping images
    - added Vue.js files

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+const cards = new Vue({
+    el: '#cards',
+    data: {
+        firstCardTitle: 'Useful Tools',
+        secondCardTitle: 'Learning Materials',
+        thirdCardTitle: 'Project Ideas',
+    }
+});
+
